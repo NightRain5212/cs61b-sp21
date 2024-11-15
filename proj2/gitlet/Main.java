b7cc15ae812763af2a7c6bc8fd6904335060fca9
NightRain5212 <498187073@qq.com>
2024-11-15 22:36:02 +0800

    modified

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -16,75 +16,80 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
+
     public static void main(String[] args) throws IOException {
-        // TODO: what if args is empty?
-        //没有参数的情况报错
-        if(args.length == 0) {
-            message("Please enter a command.");
-            System.exit(0);
-        }
-
-        String firstArg = args[0];
-        switch(firstArg) {
-            case "init":
-                // TODO: handle the `init` command
-                repo.init();
-                break;
-            case "add":
-                // TODO: handle the `add [filename]` command
-                String filename = args[1];
-                File file = join(CWD,"%s".formatted(filename));
-                repo.load();
-                repo.add(file);
-                repo.save();
-                break;
-            // TODO: FILL THE REST IN
-            case "commit":
-                repo.load();
-                String msg = args[1];
-                Date date = new Date();
-                Commit p = repo.index.getParent();
-                Commit newCommit = new Commit(msg,"n",date,p);
-                repo.commit(newCommit);
-                repo.save();
-                break;
-            case "rm":
-                break;
-            case "log":
-                repo.load();
-                repo.log();
-                break;
-            case "global-log":
-                break;
-            case "find":
-                break;
-            case "status":
-                break;
-            case "checkout":
-                repo.load();
-                if(args.length == 3){
-                    String filename1 = args[2];
-                    repo.checkout1(filename1);
-                }else if(args.length == 4) {
-                    String commitId = args[1];
-                    String filename2 = args[3];
-                    repo.checkout2(commitId,filename2);
-                }else if(args.length == 2) {
-
-                }
-                break;
-            case "branch":
-                break;
-            case "rm-branch":
-                break;
-            case "reset":
-                break;
-            case "merge":
-                break;
-            default:
-                //不存在此命令的情况
-                message("No command with that name exists.");
+        try {
+            // TODO: what if args is empty?
+            //没有参数的情况报错
+            if (args.length == 0) {
+                message("Please enter a command.");
                 System.exit(0);
+            }
+
+            String firstArg = args[0];
+            switch (firstArg) {
+                case "init":
+                    // TODO: handle the `init` command
+                    repo.init();
+                    break;
+                case "add":
+                    // TODO: handle the `add [filename]` command
+                    String filename = args[1];
+                    File file = join(CWD, "%s".formatted(filename));
+                    repo.load();
+                    repo.add(file);
+                    repo.save();
+                    break;
+                // TODO: FILL THE REST IN
+                case "commit":
+                    repo.load();
+                    String msg = args[1];
+                    Date date = new Date();
+                    Commit p = repo.index.getParent();
+                    Commit newCommit = new Commit(msg, "n", date, p);
+                    repo.commit(newCommit);
+                    repo.save();
+                    break;
+                case "rm":
+                    break;
+                case "log":
+                    repo.load();
+                    repo.log();
+                    break;
+                case "global-log":
+                    break;
+                case "find":
+                    break;
+                case "status":
+                    break;
+                case "checkout":
+                    repo.load();
+                    if (args.length == 3) {
+                        String filename1 = args[2];
+                        repo.checkout1(filename1);
+                    } else if (args.length == 4) {
+                        String commitId = args[1];
+                        String filename2 = args[3];
+                        repo.checkout2(commitId, filename2);
+                    } else if (args.length == 2) {
+
+                    }
+                    break;
+                case "branch":
+                    break;
+                case "rm-branch":
+                    break;
+                case "reset":
+                    break;
+                case "merge":
+                    break;
+                default:
+                    //不存在此命令的情况
+                    message("No command with that name exists.");
+                    System.exit(0);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
